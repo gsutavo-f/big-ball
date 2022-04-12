740def755b66f844e5d1d448678b2a8c832bc74f
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-04-11 23:22:45 -0300

    adicionados todos os grupos

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import source_data from "./json/groups.json";
 
 const getItemsStyle = (isDragging: boolean, draggableStyle: any) => ({
   padding: 10,
-  margin: `0 50px 15px 50px`,
-  background: isDragging ? "#4a2975" : "white",
-  color: isDragging ? "white" : "black",
-  border: `1px solid black`,
+  marginBottom: `0px`,
+  background: isDragging ? "#adc0b4" : "#bed6c7",
+  color: isDragging ? "#444444" : "#444444",
   fontSize: `20px`,
-  borderRadius: `5px`,
+  fontWeight: `600`,
+  borderRadius: isDragging ? `4px` : `0`,
+  textAlign: `center`,
+  width: `150px`,
 
   ...draggableStyle
 })
 
 type IndexProps = {
   index: number;
+  groupName: string;
 }
 
 export function App(props: IndexProps) {
@@ -34,31 +37,33 @@ export function App(props: IndexProps) {
   }
 
   return (
-    <div className="App">
-      <h1>Grupo</h1>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId='group'>
-          {(provided) => (
-            <div className="group" {...provided.droppableProps} ref={provided.innerRef}>
-              {group!.map(({ id, country }, index) => {
-                return (
-                  <Draggable key={id} draggableId={id} index={index}>
-                    {(provided, snapshot) => (
-                      <div ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemsStyle(snapshot.isDragging, provided.draggableProps.style)}>
-                        {country}
-                      </div>
-                    )}
-                  </Draggable>
-                )
-              })}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div className="group-card">
+      <h1>{props.groupName}</h1>
+      <div className="group">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId='group'>
+            {(provided) => (
+              <div className="group" {...provided.droppableProps} ref={provided.innerRef}>
+                {group!.map(({ id, country }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided, snapshot) => (
+                        <div ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemsStyle(snapshot.isDragging, provided.draggableProps.style)}>
+                          {country}
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,17 @@ import { App } from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <App index={0} />
-      <App index={1} />
+    <div className='group-on'>
+      <App index={0} groupName="Grupo A" />
+      <App index={1} groupName="Grupo B" />
+      <App index={2} groupName="Grupo C" />
+      <App index={3} groupName="Grupo D" />
+    </div>
+    <div className='group-on'>
+      <App index={4} groupName="Grupo E" />
+      <App index={5} groupName="Grupo F" />
+      <App index={6} groupName="Grupo G" />
+      <App index={7} groupName="Grupo H" />
     </div>
   </React.StrictMode>,
   document.getElementById('root')
